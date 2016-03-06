6d87052947fd29895257f899b2cb383a51135142
husujo <hjohnsto@u.rochester.edu>
2016-03-06 14:17:21 -0500

    Initial Commit

---
+++ githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by Hunter S. Johnston on 3/6/16.
+//  Copyright © 2016 Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  githubtest
+//
+//  Created by Hunter S. Johnston on 3/6/16.
+//  Copyright © 2016 Hunter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
